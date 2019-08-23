73c538e341bb896cb7d265ba50854ac70e9d86cb
Davor <zegaracdavor@gmail.com>
2019-08-23 15:08:34 +0200

    new lines added

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -17,4 +17,31 @@ const GithubState = props => {
     repos: [],
     loading: false
   };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  //Search users
+
+  //Get user
+
+  //Get repos
+
+  //Clear users
+
+  //Set loading
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
 };
+
+export default GithubState;
