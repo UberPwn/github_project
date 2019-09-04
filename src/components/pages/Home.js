4c535b04e95496d3f43a52d6ef0ae3146d15de09
Davor <zegaracdavor@gmail.com>
2019-09-04 10:52:56 +0200

    Completed project

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Search from '../users/Search';
+import Users from '../users/Users';
+import Alert from '../layout/Alert';
+
+const Home = () => (
+  <Fragment>
+    <Alert />
+    <Search />
+    <Users />
+  </Fragment>
+);
+export default Home;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,15 +1,16 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ setAlert }) => {
+const Search = () => {
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
   const [text, setText] = useState('');
 
   const onSubmit = e => {
     e.preventDefault();
     if (text === '') {
-      setAlert('Please enter something', 'light');
+      alertContext.setAlert('Please enter something', 'light');
     } else {
       githubContext.searchUsers(text);
       setText('');
@@ -43,12 +44,6 @@ const Search = ({ setAlert }) => {
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  // clearUsers: PropTypes.func.isRequired,
-  // showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
